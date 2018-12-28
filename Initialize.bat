ce1f1f8360e074f8155756b4c769165552ea021c
Dan-Aristide Chirileanu <danchir1@yahoo.com>
2018-12-28 13:14:59 +0200

    added file(s)

---
+++ Initialize.bat
@@ -0,0 +1,3 @@
+git config --global user.email "danchir1@yahoo.com"
+git config --global user.name "Dan-Aristide Chirileanu"
+git clone tchirileanu@vs-ssh.visualstudio.com:v3/tchirileanu/FileStrg/FileStrg
--- UploadFiles.bat
+++ UploadFiles.bat
@@ -1,6 +1,3 @@
-git config --global user.email "danchir1@yahoo.com"
-git config --global user.name "Dan-Aristide Chirileanu"
-
 git pull
 git add --all
 git commit -m "added file(s)"
