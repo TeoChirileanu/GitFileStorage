069c3fafe9da62ab1f0570e1634bb392e0c76685
test <test@test.com>
2018-12-28 13:21:57 +0200

    added file(s)

--- Initialize.bat
+++ Initialize.bat
@@ -1,3 +1,9 @@
-git config --global user.email "danchir1@yahoo.com"
-git config --global user.name "Dan-Aristide Chirileanu"
+@echo off
+
+set /p email="Enter your email:"
+git config --global user.email %email%
+
+set /p name="Enter your name:"
+git config --global user.name %name%
+
 git clone tchirileanu@vs-ssh.visualstudio.com:v3/tchirileanu/FileStrg/FileStrg
